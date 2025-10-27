e2b55f9ecf58f7d9b5f8256a7a293a6c0dd6570f
VincentConlee <vincentconlee@tamu.edu>
2025-10-27 17:02:45 -0500

    added styles

--- Project3_Client/src/App.jsx
+++ Project3_Client/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Link, useLocation } from 'react-router-dom'
-import { useState } from 'react'
 import WeatherScreen from './pages/WeatherScreen.jsx'
 import Cashier from './pages/Cashier.jsx'
 import Manager from './pages/Manager.jsx'
 import Menu from './pages/Menu.jsx'
 import Kitchen from './pages/Kitchen.jsx'
+import './styles/App.css'
+
 
 export default function App() {
   const location = useLocation();
@@ -13,26 +14,24 @@ export default function App() {
   return (
     <div>
       {/* Nav Bar */}
-      <div>
-        {showButtons && (
-          <>
-            <Link to="/weather"><button>Weather</button></Link>
-            <Link to="/cashier"><button style={{ marginLeft: '0.5rem' }}>Cashier</button></Link>
-            <Link to="/manager"><button style={{ marginLeft: '0.5rem' }}>Manager</button></Link>
-            <Link to="/menu"><button style={{ marginLeft: '0.5rem' }}>Menu</button></Link>
-            <Link to="/kitchen"><button style={{ marginLeft: '0.5rem' }}>Kitchen</button></Link>
-          </>
-        )}
+      {showButtons && (
+        <div className="home-grid">
+          <Link to="/weather"><button>Weather</button></Link>
+          <Link to="/cashier"><button>Cashier</button></Link>
+          <Link to="/manager"><button>Manager</button></Link>
+          <Link to="/menu"><button>Menu</button></Link>
+          <Link to="/kitchen"><button>Kitchen</button></Link>
+        </div>
+      )}
 
-        {/* Routing logic */}
-        <Routes>
-          <Route path="/weather" element={<WeatherScreen />} />
-          <Route path="/cashier" element={<Cashier />} />
-          <Route path="/manager" element={<Manager />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/kitchen" element={<Kitchen />} />
-        </Routes>
-      </div>
+      {/* Routing logic */}
+      <Routes>
+        <Route path="/weather" element={<WeatherScreen />} />
+        <Route path="/cashier" element={<Cashier />} />
+        <Route path="/manager" element={<Manager />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/kitchen" element={<Kitchen />} />
+      </Routes>
     </div>
   )
 }
