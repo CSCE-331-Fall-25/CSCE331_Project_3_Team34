36d4546f799fc48d0c5ad191490a53e010019598
ethanaeljford <ethanaeljford@tamu.edu>
2025-10-31 11:34:19 -0500

    progress on selecting items

--- Project3_Client/src/pages/MealAttributes.jsx
+++ Project3_Client/src/pages/MealAttributes.jsx
@@ -1,59 +1,109 @@
 import "../styles/MealAttributes.css";
 import { useState } from "react";
-export default function MealAttributes() {
+export default function MealAttributes({numEntree = 2, numSides = 1}) {
+
   //Items (this should be pulled from the database)
+  function foodItem(name, cost, calories, premium, ID, type) {
+    this.name = name;
+    this.cost = cost;
+    this.calories = calories;
+    this.premium = premium;
+    this.ID = ID;
+    this.type = type;
+  } 
 
-  
+  const [entreeList, setEntreeList] = useState(() => Array(numEntree).fill(null));
+  const [sideList, setSideList] = useState(() => Array(numSides).fill(null));
+  const [indexEntree, setIndexEntree] = useState(0);
+  const [indexSide, setIndexSide] = useState(0); 
 
-  const items = ["Orange Chicken", "Teriyaki Chicken", "Beijing Beef", "Honey Walnut Shrimp", "Black Pepper Steak", "Sesame Chicken", "Butter Chicken", "Black Pepper Angus Beef"];
+  //const items = ["Orange Chicken", "Teriyaki Chicken", "Beijing Beef", "Honey Walnut Shrimp", "Black Pepper Steak", "Sesame Chicken", "Butter Chicken", "Black Pepper Angus Beef"];
+  const items = [];
   
+  const items_sides = [];
+  const items_entrees = [];
+
+  // TODO: Use for loop to populate this list
+  items.push(new foodItem("Orange Chicken", 0.0, 400, true, 67, "entree"))
+  items.push(new foodItem("Teriyaki Chicken", 0.0, 400, true, 68, "entree"))
+
+  for (let i = 0; i < items.length; i++) {
+    if (items.at(i).type == "entree") {
+      items_entrees.push(items.at(i));
+    } else {
+      items_sides.push(items.at(i));
+    }
+  }
+
+  const finished = (indexEntree === numEntree) && (indexSide === numSides);
 
   // TODO: Replace these with actual React state or backend calls
-  const [transactionItems, setTransactionItems] = useState([]);
-  const handleChooseItem = (e) => {
-    //console.log("Item Button ID: " + e.target.id);
-    fetch("http://localhost:5000/api/buy-item", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ itemID: e.target.id }),
-     
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          console.log("Item bought:", e.target.id);
-          setTransactionItems((prev) => [
-            ...prev,
-            { cost: data.cost, item: data.item, type: "main" },
-            ...data.entrees.map((entree) => ({ item: entree, type: "entree" })),
-            ...data.side.map((side) => ({ item: side, type: "side" }))
-          ]);
-          setCurrCost((prev) => prev + data.cost);
-        }
-        //console.log("Cost is: ", data.cost)
-      });
+  const handleFinishSelection = () => {
+    if (finished) {
+      fetch("http://localhost:5000/api/buy-item", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ entreeList, sideList }),
+      })
+    }
   };
+
+  const selectEntree = (item) => {
+    const updated = [...entreeList];
+    if (indexEntree < numEntree) {
+      updated[indexEntree] = item;
+        
+      setEntreeList(updated);
+      setIndexEntree(indexEntree + 1)
+
+      console.log("Item Added: " + item.name);
+    } else {
+      console.log("No more slots")
+    }
+
+    console.log("Current Entree List and index: ")
+    updated.forEach((e) => console.log(e ? e.name : "empty"));
+    console.log("Slots Left: " + indexEntree + "/" + numEntree);
+  }
+
+  const selectSide = (item) => {
+    const updated_side = [...sideList];
+    if (indexSide < numSide) {
+      updated_side[indexSide] = item;
+        
+      setSideList(updated_side);
+      setIndexSide(indexSide + 1)
+
+      console.log("Item Added: " + item.name);
+    } else {
+      console.log("No more slots")
+    }
+
+    console.log("Current Side List and index: ")
+    updated_side.forEach((e) => console.log(e ? e.name : "empty"));
+    console.log("Slots Left: " + indexSide + "/" + numSide);
+  }
+
   
-  //const handleViewReports = () => console.log("View reports");
-  
-  const rows = [];
-  for (let i = 0; i < items.length; i += 4) {
-    rows.push(items.slice(i, i + 4));
+  const rows_entree = [];
+  for (let i = 0; i < items_entrees.length; i += 5) {
+    rows_entree.push(items_entrees.slice(i, i + 5));
   }
 
   return (
     <div className="p-4 space-y-3 container">
 
-      {rows.map((row, rowIndex) => (
+      {rows_entree.map((row, rowIndex) => (
         <div key={rowIndex} className={`menu-row ${rowIndex > 0 ? 'spaced' : ''}`}>
           {row.map((item, itemIndex) => (
             <button
               key={itemIndex}
-              id={item}
+              id={item.name}
               className="buy-button"
-              onClick={() => console.log("The item is: " + item)}
+              //onClick={() => console.log("The item is: " + item.name + " and it costs this much: " + item.cost)}
+              onClick={() => selectEntree(item)}
             >
-              {item}
+              {item.name}
             </button>
           ))}
         </div>
